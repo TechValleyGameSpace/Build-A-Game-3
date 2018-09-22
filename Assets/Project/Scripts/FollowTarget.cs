a5cd116bc95af588cabb8fc3000313327bb8690a
Taro Omiya <taro.omiya@omiyagames.com>
2018-09-21 21:26:23 -0400

    Starting with a camera perspective prototype.

---
+++ Assets/Project/Scripts/FollowTarget.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Project
+{
+    public class FollowTarget : MonoBehaviour
+    {
+        [SerializeField]
+        Transform target;
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+            transform.position = target.position;
+        }
+    }
+}
---
+++ Assets/Project/Scripts/PlayerMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Project
+{
+    [RequireComponent(typeof(Rigidbody))]
+    public class PlayerMove : MonoBehaviour
+    {
+        public enum Axis
+        {
+            XY,
+            XZ,
+            YZ
+        }
+
+        [SerializeField]
+        Axis axis;
+
+        Rigidbody body;
+
+        // Use this for initialization
+        void Start()
+        {
+            body = GetComponent<Rigidbody>();
+        }
+
+        // Update is called once per frame
+        void FixedUpdate()
+        {
+
+        }
+    }
+}
